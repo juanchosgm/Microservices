9136c84e59acd77a8b2e9d91afa36b17e0d33a34
Juan Gonzalez <juanchosgm@hotmail.com>
2021-08-27 11:53:07 -0500

    Main configuration over CuoponAPI

---
+++ Microservices.Services.CuoponAPI/MappingConfig.cs
@@ -0,0 +1,14 @@
+﻿
+using AutoMapper;
+
+namespace Microservices.Services.CouponAPI;
+public class MappingConfig
+{
+    public static MapperConfiguration RegisterMaps()
+    {
+        MapperConfiguration? mappingConfig = new(config =>
+        {
+        });
+        return mappingConfig;
+    }
+}
