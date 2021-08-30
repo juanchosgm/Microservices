5fb6bdcf32bdff44b24f17ffc7476d3df7f55eec
Juan Gonzalez <juanchosgm@hotmail.com>
2021-08-30 14:49:56 -0500

    Creation of OrderAPI project

---
+++ Microservices.Services.OrderAPI/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+namespace Microservices.Services.OrderAPI.DbContexts;
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+}
---
+++ Microservices.Services.OrderAPI/MappingConfig.cs
@@ -0,0 +1,14 @@
+﻿
+using AutoMapper;
+
+namespace Microservices.Services.OrderAPI;
+public class MappingConfig
+{
+    public static MapperConfiguration RegisterMaps()
+    {
+        MapperConfiguration? mappingConfig = new(config =>
+        {
+        });
+        return mappingConfig;
+    }
+}
